7aa20a2520c1b676a69b420b9314d9669ecde763
FTRESfghf <129573464+FTRESfghf@users.noreply.github.com>
2024-03-30 05:35:31 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || '986e0d08-b275-4dd3-9e75-f3094b36fa2a'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.chmhmhsre.cf';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || '{"AccountTag":"d32e838527be79b037e8d3c57c0ea55a","TunnelSecret":"9vMsUtqGg06ITl8RUZJMXLIm5KU13mkeQkEfaR1WHYM=","TunnelID":"a6cfd739-534e-40c8-8e56-dbeab9e1f348"}';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.ecnaksv.eu.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || '{"AccountTag":"40328dc83927886e4be9fafbdb8731ce","TunnelSecret":"DtlOE//WOkflQjqdDGFu5xRu9g6Fl227lhoMI5Vjr1o=","TunnelID":"0086d54c-78b7-463a-b556-44e557df2a4a"}';
 const CFIP = process.env.CFIP || 'government.se';
 const NAME = process.env.NAME || 'Choreo';
 
